6cdc7708d61f9f82cb625b73943c976710cd2eff
Axel Yoliston Vasquez Herrera <axelvasquezherrera@outlook.com>
2020-09-05 01:19:42 -0500

    routes ok

--- Angular De cero a experto - Udemy/04-spotifyapp/src/app/app-routing.module.ts
+++ Angular De cero a experto - Udemy/04-spotifyapp/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { ArtistaComponent } from './components/artista/artista.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'artista',
+    component: ArtistaComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
